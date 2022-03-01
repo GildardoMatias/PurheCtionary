901c67674d1ff47ba9becb1f81f9641bd456b697
JesusZacarias <59021713+JesusZacarias@users.noreply.github.com>
2022-02-28 23:52:17 -0600

    on Tap en los Cards

--- lib/screens/segments/sections/objetos.dart
+++ lib/screens/segments/sections/objetos.dart
@@ -5,10 +5,41 @@ class Objetos extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BasePage(
+      title: 'Objetos comunes',
       container: Container(
-        padding: EdgeInsets.all(10.0),
-        alignment: Alignment.center,
-        child: Text('hola'),
+          color: Colors.amber,
+          alignment: Alignment.topCenter,
+          child: ListWheelViewExample()),
+    );
+  }
+}
+
+class ListWheelViewExample extends StatefulWidget {
+  const ListWheelViewExample({Key key}) : super(key: key);
+
+  @override
+  _ListWheelViewExampleState createState() => _ListWheelViewExampleState();
+}
+
+class _ListWheelViewExampleState extends State<ListWheelViewExample> {
+  @override
+  Widget build(BuildContext context) {
+    return ListWheelScrollView.useDelegate(
+      itemExtent: 75,
+      childDelegate: ListWheelChildBuilderDelegate(
+        builder: (BuildContext context, int index) {
+          if (index < 0 || index > 9) {
+            return null;
+          }
+          return ListTile(
+            leading: Text(
+              "$index",
+              style: TextStyle(fontSize: 50),
+            ),
+            title: Text("Title $index"),
+            subtitle: Text('Description here'),
+          );
+        },
       ),
     );
   }
