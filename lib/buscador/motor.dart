d7e253647296fa446857231156b07c92eec0b0b2
GildardoMatias <Gildardo.358358@gmail.com>
2022-02-28 23:06:28 -0600

    Motor

--- lib/buscador/motor.dart
+++ lib/buscador/motor.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 class motor {
-  // String query;
-  // motor(this.query);
+  // A perro ya andas programando
+  final dict =
+      '{{"originario": {"purepecha": "Anapuesti","tipo": "adjetivo","español": "Es originario de…"}},{"Trabajo": {"purepecha": "ánchekorheta","tipo": "sustantivo","español": "Trabajo, actividad, empleo, trabajo realizado."}}}';
+  // final user = jsonDecode(dict);
+  // var res = "";
   buscar(query) {
     return "buscaste: " + query;
   }
